234710a3a366c3d0a6a8d5bb1c261bfe13824fc2
David Masip <davidmasip16@gmail.com>
2023-01-31 15:36:35 +0100

    Create index.js
    Home Route created

---
+++ survival-tanks/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Home() {
+
+    
+    return (
+        <div>
+            <h1 className="App-title">Giffy Store</h1>
+        </div>
+    );
+}
+
+export default Home;
